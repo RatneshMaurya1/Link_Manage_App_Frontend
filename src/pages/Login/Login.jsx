a5c4a9df692ff86fcf919e3c329cd0eeb32f544d
RatneshMaurya1 <mauryaratnesh022@gmail.com>
2025-01-23 15:01:40 +0530

    added signup and signin functionality

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,13 +3,52 @@ import styles from "./login.module.css";
 import loginImg from "../../assets/login.png";
 import cuvetteImg from "../../assets/cuvette.png";
 import { useNavigate } from "react-router-dom";
+import { userSignIn } from "../../services";
+import toast from "react-hot-toast";
 
 const Register = () => {
   const navigate = useNavigate()
+  const [loading,setLoading] = useState(false)
   const [formData,setFormData] = useState({
     email:"",
     password:"",
   })
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email.trim()) {
+      return toast.error("Email is required");
+    } else if (!formData.email.includes("@") || !formData.email.includes(".")) {
+      return toast.error("Email is invalid");
+    }
+    const regex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!formData.password.trim()) {
+      return toast.error("Password is required");
+    } else if (!regex.test(formData.password)) {
+      return toast.error("Password must be at least 8 characters long and include uppercase, lowercase, numbers, and symbols");
+    }
+    setLoading(true)
+    try {
+      const response = await userSignIn(formData);
+      if (response.message === "Logged in successfully") {
+        toast.success(response.message);
+        setFormData({
+          email: "",
+          password: "",
+        });
+        localStorage.setItem("token",response.token)
+        localStorage.setItem("userId",response.user._id)
+      } else {
+        toast.error(response.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }finally{
+      setLoading(false)
+    }
+  };
+
   return (
     <>
       <div className={styles.container}>
@@ -28,10 +67,10 @@ const Register = () => {
           </div>
           <p className={styles.join}>Login</p>
           <div className={styles.form}>
-          <form>
-            <input type="text" placeholder="Email id" value={formData.email} onchange={(e) => setFormData({...formData,email:e.target.value})}/>
-            <input type="tel" placeholder="Password" value={formData.password} onchange={(e) => setFormData({...formData,password:e.target.value})}/>
-            <button type="submit">Register</button>
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Email id" value={formData.email} onChange={(e) => setFormData({...formData,email:e.target.value})}/>
+            <input type="tel" placeholder="Password" value={formData.password} onChange={(e) => setFormData({...formData,password:e.target.value})}/>
+            <button type="submit" disabled={loading}>{loading ? "Loading" : "Login"}</button>
           </form>
           <h1 className={styles.account}>Don’t have an account?<span onClick={() => navigate("/register")}> SignUp</span></h1>
           </div>
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,9 +3,12 @@ import styles from "./register.module.css";
 import loginImg from "../../assets/login.png";
 import cuvetteImg from "../../assets/cuvette.png";
 import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import { userSignUp } from "../../services";
 
 const Register = () => {
   const navigate = useNavigate()
+  const [loading,setLoading] = useState(false)
   const [formData,setFormData] = useState({
     name:"",
     email:"",
@@ -13,6 +16,57 @@ const Register = () => {
     password:"",
     confirmPassword:""
   })
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.name.trim()) {
+      return toast.error("Name is required");
+    }
+    if (!formData.email.trim()) {
+      return toast.error("Email is required");
+    } else if (!formData.email.includes("@") || !formData.email.includes(".")) {
+      return toast.error("Email is invalid");
+    }
+    if (!formData.phone.trim()) {
+      return toast.error("Phone number is required");
+    }else if(!/^\d{10}$/.test(formData.phone)){
+      return toast.error("Phone number is invalid")
+    }
+    const regex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  if (!formData.password.trim()) {
+    return toast.error("Password is required");
+  } else if (!regex.test(formData.password)) {
+    return toast.error("Password must be at least 8 characters long and include uppercase, lowercase, numbers, and symbols");
+  }
+    if (!formData.confirmPassword.trim()) {
+        return toast.error("Confirm password is required");
+      }
+      if(formData.password !== formData.confirmPassword){
+        return toast.error("enter same password in both fields")
+      }
+    setLoading(true)
+    try {
+      const response = await userSignUp(formData);
+      if (response.message === "Sign up successfully") {
+        toast.success(response.message);
+        setFormData({
+            name:"",
+            email:"",
+            phone:"",
+            password:"",
+            confirmPassword:""
+        });
+        navigate(`/login`)
+      } else {
+        toast.error(response.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }finally{
+      setLoading(false)
+    }
+  };
   return (
     <>
       <div className={styles.container}>
@@ -31,13 +85,13 @@ const Register = () => {
           </div>
           <p className={styles.join}>Join us Today!</p>
           <div className={styles.form}>
-          <form>
-            <input type="text" placeholder="Name" value={formData.name} onchange={(e) => setFormData({...formData,name:e.target.value})}/>
-            <input type="text" placeholder="Email id" value={formData.email} onchange={(e) => setFormData({...formData,email:e.target.value})}/>
-            <input type="tel" placeholder="Mobile no." value={formData.phone} onchange={(e) => setFormData({...formData,phone:e.target.value})}/>
-            <input type="tel" placeholder="Password" value={formData.password} onchange={(e) => setFormData({...formData,password:e.target.value})}/>
-            <input type="tel" placeholder="Confirm Password" value={formData.confirmPassword} onchange={(e) => setFormData({...formData,confirmPassword:e.target.value})}/>
-            <button type="submit">Register</button>
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" value={formData.name} onChange={(e) => setFormData({...formData,name:e.target.value})}/>
+            <input type="text" placeholder="Email id" value={formData.email} onChange={(e) => setFormData({...formData,email:e.target.value})}/>
+            <input type="tel" placeholder="Mobile no." value={formData.phone} onChange={(e) => setFormData({...formData,phone:e.target.value})}/>
+            <input type="tel" placeholder="Password" value={formData.password} onChange={(e) => setFormData({...formData,password:e.target.value})}/>
+            <input type="tel" placeholder="Confirm Password" value={formData.confirmPassword} onChange={(e) => setFormData({...formData,confirmPassword:e.target.value})}/>
+            <button type="submit" disabled={loading}>{loading ? "Loading..." : "Register"}</button>
           </form>
           <h1 className={styles.account}>Already have an account? <span onClick={() => navigate("/login")}>Login</span></h1>
           </div>
